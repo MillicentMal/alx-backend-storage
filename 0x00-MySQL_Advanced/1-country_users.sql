b32bd62be2e1b6506b058fc48c32906c8d4a2b3b
MillicentMal <millicentmalinga19@gmail.com>
2022-01-22 13:22:03 +0000

    Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Using ENUM command
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN'),
+PRIMARY KEY (id)
+);
