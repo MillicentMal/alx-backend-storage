0d20d3ead3cc503da06b62e3b81fafab4deb4b39
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-01-22 17:18:42 +0200

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- Pulling data from existing table
-SELECT origin, SUM(fans) AS nb_fans  GROUP BY origin ORDER BY fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands  GROUP BY origin ORDER BY fans DESC;
