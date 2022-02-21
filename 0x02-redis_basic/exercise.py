36cb9fb1bc6144c938ead70ea01ec7e2268a0663
MillicentMal <millicemalinga19@gmail.com>
2022-02-21 09:42:41 +0000

    Task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         ki = str(uuid4())
         self._redis.set(ki, data)
         return ki 
