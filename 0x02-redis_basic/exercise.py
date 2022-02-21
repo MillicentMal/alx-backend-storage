fa8cc7436ed81eb119b981da04b37cf32ab5ed1e
MillicentMal <millicemalinga19@gmail.com>
2022-02-21 09:33:36 +0000

    Task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+"""
+Creating a Cache class in Redis
+"""
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """
+    Cache cls
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        ki = str(uuid4())
+        self._redis.set(str(uuid4()), str(data))
+        return ki 
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
