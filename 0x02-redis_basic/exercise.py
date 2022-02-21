166feec04ecb2d4c6e74bf694694109d19077775
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-02-21 13:48:27 +0200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,10 +10,11 @@ from functools import wraps
 
 def count_calls(method : Callable ) -> Callable:
     key = method.__qualname__
+    
     @wraps(method)
     def wrapper(self, args, **kwds):
         self._redis.incr(key)
-        return method(self, *args)
+        return method(self, *args, **kwds)
     return wrapper
 
 def call_history(method : Callable) -> Callable:
